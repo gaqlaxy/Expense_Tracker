7ee59b2f128c747c323f5e267db9ff1cbafe838c
gaqlaxy <v4vripper@gmail.com>
2025-08-05 05:51:26 +0530

    config

--- expense_tracker/vite.config.js
+++ expense_tracker/vite.config.js
@@ -11,39 +11,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from "@tailwindcss/vite";
-import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Expense Tracker',
-        short_name: 'Expenses',
-        description: 'Track your expenses and budget with ease.',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        start_url: '/',
-        icons: [
-          {
-            src: 'tracker.jpg',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'tracker.jpg',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        sourcemap: true,
-      },
-    })
+
   ],
 });
